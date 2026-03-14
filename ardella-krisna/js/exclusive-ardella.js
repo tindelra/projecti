188f4528ac138be3bada25d46393dcc529734ee4
ardellay <ardellay@multisoft.co.id>
2026-03-14 13:31:53 +0700

    bugs fixing

--- ardella-krisna/js/exclusive-ardella.js
+++ ardella-krisna/js/exclusive-ardella.js
@@ -9,30 +9,40 @@ var photo_nav_options = {
     arrows: false,
     adaptiveHeight: false,
     infinite: true,
-    useTransform: false,
-    lazyLoad: 'ondemand',
+    useTransform: true,
     speed: 500,
     cssEase: "ease",
-    asNavFor: $(".photo-slider"),
+    asNavFor: ".photo-slider",
 };
 
 // Photo Options Slider
 var photo_slider_options = {
     centerMode: true,
     swipeToSlide: true,
-    slidesToShow: 1,
+    slidesToShow: 5,
     slidesToScroll: 1,
-    fade: false,
-    arrows: true,
-    adaptiveHeight: false,
+    variableWidth: false,
     infinite: true,
-    useTransform: false,
-    lazyLoad: 'ondemand',
+    useTransform: true,
     speed: 500,
     cssEase: "ease",
     prevArrow: $(".photo-arrow.prev"),
     nextArrow: $(".photo-arrow.next"),
-    asNavFor: $(".photo-nav"),
+    asNavFor: ".photo-nav",
+    responsive: [
+        {
+            breakpoint: 960,
+            settings: {
+                slidesToShow: 4
+            }
+        },
+        {
+            breakpoint: 560,
+            settings: {
+                slidesToShow: 3
+            }
+        }
+    ]
 };
 
 // Resize Photo Nav
@@ -62,27 +72,36 @@ var resize_photo_nav = function () {
 $(document).ready(function () {
     resize_photo_nav();
 
+    if ($(".photo-slider").children().length > 0) {
+        // Initialize Nav (Thumbnails) first for better sync
+        $(".photo-slider").slick(photo_slider_options);
+    }
+
     if ($(".photo-nav").children().length > 0) {
-        // Slick
+        // Initialize Main Slider second
         $(".photo-nav").slick(photo_nav_options);
     }
 
-    if ($(".photo-slider").children().length > 0) {
-        // Slick
-        var $slider = $(".photo-slider");
-        $slider.slick(photo_slider_options);
+    $(".photo-slider").on("click", ".slick-slide", function () {
+        var index = $(this).attr("data-slick-index");
+        $(".photo-nav").slick("slickGoTo", parseInt(index));
+    });
 
-        // Mousewheel Scroll support
+    if ($(".photo-slider").children().length > 0) {
+        var $slider = $(".photo-slider");
+
         $slider.on('wheel', function (e) {
             e.preventDefault();
-            if (e.originalEvent.deltaY > 0) {
+
+            const delta = e.originalEvent.deltaY || e.originalEvent.deltaX;
+
+            if (delta > 0) {
                 $(this).slick('slickNext');
             } else {
                 $(this).slick('slickPrev');
             }
         });
     }
-
     // RSVP Guest Counter (Handled in template.js)
 
     // RSVP Radio Button Active State
